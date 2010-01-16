bdd4370f3177c321a146a89b7bed1584c2e0d57a
Dmitri Popov <pin@pin-laptop.(none)>
2010-01-16 19:45:54 +0300

    first public release

--- lib/XML/Atom/Ext/Inline.pm
+++ lib/XML/Atom/Ext/Inline.pm
@@ -18,11 +18,11 @@ XML::Atom::Ext::Inline - In-lining Extensions for Atom
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01_02';
+our $VERSION = '0.02';
 
 BEGIN {
 	XML::Atom::Link->mk_object_accessor(inline => 'XML::Atom::Ext::Inline');
